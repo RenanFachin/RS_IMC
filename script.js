dc3af433226a0984fd697b0bc70fb720b6668cf6
Renan <renan.fachin@hotmail.com>
2022-08-13 17:52:26 -0300

    Início da manipulação da DOM. Pegando os valores e atribuindo a constantes com o JS, criada uma função para capturar o que foi digitado pelo usuário nos campos de input de peso e altura, além de usar o preventDefault() para fazer com que a página não tenha seu comportamento padrão de recarregamento

---
+++ script.js
@@ -0,0 +1,26 @@
+// variáveis
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+// Fazendo o submit do formulário
+// 3 maneiras de criar e atribuir a um evento
+// maneira 1: form.onsubmit = function() {}
+
+// maneira 2: form.onsubmit = () => {}
+
+// maneira 3: form.onsubmit = handleSubmit 
+
+// Na maneira 3, teria que se criar uma função chamada handleSubmit
+// function handleSubmit(){}
+
+form.onsubmit = function(event) {
+    // Fazendo o submit não ter o seu funcionamento natural
+    // Fazendo com que o formulário não seja recarregado totalmente a cada submit
+    // O event foi criado só para poder atribuir esse preventDefault
+    event.preventDefault();
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+    // .value é o jeito com que pegamos o que digitado no input
+}
