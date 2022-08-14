3cff7175387c4defc744516d40b9c1237038619b
Renan <renan.fachin@hotmail.com>
2022-08-14 00:42:48 -0300

    Melhorias no código e finalização do desafio

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,9 @@ import { Modal } from './modal.js'
 import { calculateIMC, notANumber } from './utils.js'
 import { AlertError } from './alert-error.js'
 
-
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
 
 form.onsubmit = function(event) {
     event.preventDefault();
@@ -27,10 +25,21 @@ form.onsubmit = function(event) {
     displayResultMessage(result)
 }
 
-
 function displayResultMessage(result){
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = `${message}`
     Modal.open()
+}
+
+
+// DESAFIO
+// Fechar a janela de erro ao digitar no campo novamente
+//.oninput vai perceber se está sendo alterado algo no input e vai disparar a função
+inputWeight.oninput = () => {
+    AlertError.close()
+}
+
+inputHeight.oninput = () => {
+    AlertError.close()
 }
